709d08745860ee6abd4e8949c10f246d4bae9ac8
stavbers <reddevweb@gmail.com>
2023-07-11 16:27:22 +0300

    add preloader comp

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getHomePage } from '../../api';
-
+import { Preloader } from '../../components/Preloader';
 
 function Home(){
 return (
